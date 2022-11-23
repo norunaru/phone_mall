9c7f8bbd10465fe3550eee5a831836b479270a90
unknown <nwy98@naver.com>
2022-11-23 20:21:11 +0900

    Revenue section finished

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { FaBolt } from "react-icons/fa";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { Navbar, Sidebar, Footer } from "./components";
+
+import { Login, Mypage, Sell } from "./pages";
+
+const App = () => {
+  const active = true;
+  return (
+    <div>
+      <BrowserRouter>
+        {active ? (
+          <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+            <Sidebar />
+          </div>
+        ) : (
+          <div className="w-0 dark:bg-secondary-dark-bg">
+            <Sidebar />
+          </div>
+        )}
+
+        <div>
+          <FaBolt />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/index.jsx
@@ -0,0 +1,3 @@
+export { default as Login } from "./Login";
+export { default as Mypage } from "./Mypage";
+export { default as Sell } from "./Sell";
